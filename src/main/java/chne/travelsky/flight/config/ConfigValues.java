3ba5464b0b20c8890875891238b9415cb3e4ac34
lovejwj2004 <lovejwj2004@gmail.com>
2016-06-05 18:06:58 +0800

    add: add properties Config Class and Test Case

---
+++ src/main/java/chne/travelsky/flight/config/ConfigValues.java
@@ -0,0 +1,30 @@
+package chne.travelsky.flight.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by lovejwj2004 on 2016/6/5.
+ */
+@Component
+@ConfigurationProperties(prefix="connection",locations = "classpath:config/test.properties")
+public class ConfigValues {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    private String name;
+    private String url;
+}
--- src/test/java/chne/travelsky/flight/properties/test/PropertiesTest.java
+++ src/test/java/chne/travelsky/flight/properties/test/PropertiesTest.java
@@ -1,12 +1,27 @@
 package chne.travelsky.flight.properties.test;
 
+import chne.travelsky.flight.FlightApplication;
+import chne.travelsky.flight.config.ConfigValues;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 
 /**
  * Created by lovejwj2004 on 2016/6/2.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = FlightApplication.class)
+@TestPropertySource(locations = "classpath:config/test.properties")
 public class PropertiesTest {
+    @Autowired
+    ConfigValues config;
+
     @Test
     public void testPropertiesAnnoation(){
+        System.out.println(config.getName());
     }
 }
